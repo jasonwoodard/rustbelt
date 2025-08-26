a1598fb5755e1e5afe7a69ba1f6250bf449a91f0
Jason Woodard <me@jasonwoodard.com>
2025-08-25 23:47:01 -0400

    feat: add KML output

---
+++ src/io/emitKml.ts
@@ -0,0 +1,37 @@
+import type { DayPlan } from '../types';
+
+function escapeXml(s: string): string {
+  return s
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&apos;');
+}
+
+/** Serialize itinerary stops to KML. */
+export function emitKml(days: DayPlan[]): string {
+  const placemarks: string[] = [];
+  const routeCoords: string[] = [];
+  for (const day of days) {
+    for (const stop of day.stops) {
+      placemarks.push(
+        `<Placemark><name>${escapeXml(stop.name)}</name><Point><coordinates>${stop.lon},${stop.lat},0</coordinates></Point></Placemark>`,
+      );
+      routeCoords.push(`${stop.lon},${stop.lat},0`);
+    }
+  }
+  const route = `<Placemark><name>Route</name><LineString><coordinates>${routeCoords.join(' ')}</coordinates></LineString></Placemark>`;
+  const doc = [
+    '<?xml version="1.0" encoding="UTF-8"?>',
+    '<kml xmlns="http://www.opengis.net/kml/2.2">',
+    '<Document>',
+    ...placemarks,
+    route,
+    '</Document>',
+    '</kml>',
+  ];
+  return doc.join('\n');
+}
+
+export default emitKml;
