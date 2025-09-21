91c1ee25e053988b9ee9d748c81044c6e2e59c56
Jason Woodard <me@jasonwoodard.com>
2025-09-20 21:22:47 -0400

    add BU and RX stores (#124)
    Co-authored-by: Jason W <jasonwoodard@gmail.com>

--- itineraries/manifest.js
+++ itineraries/manifest.js
@@ -21,7 +21,34 @@ window.TRIP_MANIFEST = {
             CSV: "detroit-to-buffalo-2025/itinerary-DT-v2-20250917T0857.csv",
             JSON: "detroit-to-buffalo-2025/itinerary-DT-v2-20250917T0857.json",
           }
-        }
+        },
+        {
+          day: "Detroit → Cleveland",
+          outputs: {
+            HTML: "detroit-to-buffalo-2025/itinerary-CL-v1-20250916T0820.html",
+            KML: "detroit-to-buffalo-2025/itinerary-CL-v1-20250916T0820.kml",
+            CSV: "detroit-to-buffalo-2025/itinerary-CL-v1-20250916T0820.csv",
+            JSON: "detroit-to-buffalo-2025/itinerary-CL-v1-20250916T0820.json",
+          }
+        },
+        {
+          day: "Cleveland → Buffalo ",
+          outputs: {
+            HTML: "detroit-to-buffalo-2025/itinerary-BU-v2-20250918T0043.html",
+            KML: "detroit-to-buffalo-2025/itinerary-BU-v2-20250918T0043.kml",
+            CSV: "detroit-to-buffalo-2025/itinerary-BU-v2-20250918T0043.csv",
+            JSON: "detroit-to-buffalo-2025/itinerary-BU-v2-20250918T0043.json",
+          }
+        },
+        {
+          day: "Buffalo → Roxbury",
+          outputs: {
+            HTML: "detroit-to-buffalo-2025/itinerary-RX-v2-20250918T0218.html",
+            KML: "detroit-to-buffalo-2025/itinerary-RX-v2-20250918T0218.kml",
+            CSV: "detroit-to-buffalo-2025/itinerary-RX-v2-20250918T0218.csv",
+            JSON: "detroit-to-buffalo-2025/itinerary-RX-v2-20250918T0218.json",
+          }
+        },
       ]
     }
   ]
