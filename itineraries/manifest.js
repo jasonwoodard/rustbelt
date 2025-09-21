20cd3acbf514e97393d0c7a941223da834e21763
Jason W <jasonwoodard@gmail.com>
2025-09-21 01:50:28 -0400

    final outputs

--- itineraries/manifest.js
+++ itineraries/manifest.js
@@ -7,46 +7,46 @@ window.TRIP_MANIFEST = {
         {
           day: "Ann Arbor",
           outputs: {
-            HTML: "detroit-to-buffalo-2025/itinerary-AA-v1-20250921T0003-pretrip.html",
-            KML: "detroit-to-buffalo-2025/itinerary-AA-v1-20250921T0003-pretrip.kml",
-            CSV: "detroit-to-buffalo-2025/itinerary-AA-v1-20250921T0003-pretrip.csv",
-            JSON: "detroit-to-buffalo-2025/itinerary-AA-v1-20250921T0003-pretrip.json",
+            HTML: "detroit-to-buffalo-2025/itinerary-AA-v1-20250921T0143-pretrip.html",
+            KML: "detroit-to-buffalo-2025/itinerary-AA-v1-20250921T0143-pretrip.kml",
+            CSV: "detroit-to-buffalo-2025/itinerary-AA-v1-20250921T0143-pretrip.csv",
+            JSON: "detroit-to-buffalo-2025/itinerary-AA-v1-20250921T0143-pretrip.json",
           }
         },
         {
           day: "Detroit",
           outputs: {
-            HTML: "detroit-to-buffalo-2025/itinerary-DT-v2-20250921T0013-pretrip.html",
-            KML: "detroit-to-buffalo-2025/itinerary-DT-v2-20250921T0013-pretrip.kml",
-            CSV: "detroit-to-buffalo-2025/itinerary-DT-v2-20250921T0013-pretrip.csv",
-            JSON: "detroit-to-buffalo-2025/itinerary-DT-v2-20250921T0013-pretrip.json",
+            HTML: "detroit-to-buffalo-2025/itinerary-DT-v2-20250921T0145-pretrip.html",
+            KML: "detroit-to-buffalo-2025/itinerary-DT-v2-20250921T0145-pretrip.kml",
+            CSV: "detroit-to-buffalo-2025/itinerary-DT-v2-20250921T0145-pretrip.csv",
+            JSON: "detroit-to-buffalo-2025/itinerary-DT-v2-20250921T0145-pretrip.json",
           }
         },
         {
           day: "Detroit → Cleveland",
           outputs: {
-            HTML: "detroit-to-buffalo-2025/itinerary-CL-v1-20250921T0016-pretrip.html",
-            KML: "detroit-to-buffalo-2025/itinerary-CL-v1-20250921T0016-pretrip.kml",
-            CSV: "detroit-to-buffalo-2025/itinerary-CL-v1-20250921T0016-pretrip.csv",
-            JSON: "detroit-to-buffalo-2025/itinerary-CL-v1-20250921T0016-pretrip.json",
+            HTML: "detroit-to-buffalo-2025/itinerary-CL-v1-20250921T0145-pretrip.html",
+            KML: "detroit-to-buffalo-2025/itinerary-CL-v1-20250921T0145-pretrip.kml",
+            CSV: "detroit-to-buffalo-2025/itinerary-CL-v1-20250921T0145-pretrip.csv",
+            JSON: "detroit-to-buffalo-2025/itinerary-CL-v1-20250921T0145-pretrip.json",
           }
         },
         {
           day: "Cleveland → Buffalo ",
           outputs: {
-            HTML: "detroit-to-buffalo-2025/itinerary-BU-v2-20250921T0018-pretrip.html",
-            KML: "detroit-to-buffalo-2025/itinerary-BU-v2-20250921T0018-pretrip.kml",
-            CSV: "detroit-to-buffalo-2025/itinerary-BU-v2-20250921T0018-pretrip.csv",
-            JSON: "detroit-to-buffalo-2025/itinerary-BU-v2-20250921T0018-pretrip.json",
+            HTML: "detroit-to-buffalo-2025/itinerary-BU-v2-20250921T0146-pretrip.html",
+            KML: "detroit-to-buffalo-2025/itinerary-BU-v2-20250921T0146-pretrip.kml",
+            CSV: "detroit-to-buffalo-2025/itinerary-BU-v2-20250921T0146-pretrip.csv",
+            JSON: "detroit-to-buffalo-2025/itinerary-BU-v2-20250921T0146-pretrip.json",
           }
         },
         {
           day: "Buffalo → Roxbury",
           outputs: {
-            HTML: "detroit-to-buffalo-2025/itinerary-RX-v1-20250921T0019-pretrip.html",
-            KML: "detroit-to-buffalo-2025/itinerary-RX-v1-20250921T0019-pretrip.kml",
-            CSV: "detroit-to-buffalo-2025/itinerary-RX-v1-20250921T0019-pretrip.csv",
-            JSON: "detroit-to-buffalo-2025/itinerary-RX-v1-20250921T0019-pretrip.json",
+            HTML: "detroit-to-buffalo-2025/itinerary-RX-v1-20250921T0146-pretrip.html",
+            KML: "detroit-to-buffalo-2025/itinerary-RX-v1-20250921T0146-pretrip.kml",
+            CSV: "detroit-to-buffalo-2025/itinerary-RX-v1-20250921T0146-pretrip.csv",
+            JSON: "detroit-to-buffalo-2025/itinerary-RX-v1-20250921T0146-pretrip.json",
           }
         },
       ]
